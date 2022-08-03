3fc797c059e6542c55048aea6423fdf564c54c7e
Yoshiki Ohshima <yoshiki.ohshima@acm.org>
2022-08-03 09:58:51 -0700

    use the short name for world name in options, and make sure a vrse file missing UserBehaviorDirectory can be loaded

--- src/main/microverse.js
+++ src/main/microverse.js
@@ -652,5 +652,5 @@ export async function startMicroverse() {
         }
     };
     // Default parameters are filled in the body of startWorld. You can override them.
-    startWorld(apiKeysModule.default, `${basedir}/${basename}`);
+    startWorld(apiKeysModule.default, basename);
 }
